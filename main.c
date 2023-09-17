0c0c382f79df3cef3ce2063a44590a2424afab1a
LogXx <awesome149712@gmail.com>
2023-09-17 20:29:21 +0400

    colorify

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #include "fsm.h"
+#include "mydefs.h"
 
 #define FSM_FILE "input.txt"
 
@@ -25,58 +26,63 @@ int main()
 
     printf("Read automaton:\n");
     fsm_spec_output(spec);
-    struct FSM_Array *automata = fsm_array_create();
-    fsm_array_add(automata, fsm_create(&spec));
+    struct FSM_Array *automata;
 
-    printf("Now enter string.\n");
-    printf("> ");
     char inp[LINE_SIZE];
-    char *input = fgets(inp, LINE_SIZE, stdin);
-    if (input == NULL)
+    char *input;
+    while (1)
     {
-        fprintf(stderr, "Got no input.\n");
-        return 1;
-    }
-    inp[strcspn(inp, "\n")] = '\0';
-
-    int failed_count = 0;
-
-    while (*input)
-    {
-        printf("========\n");
-        printf("Step %d:\n", input - inp + 1);
-        printf("Passing character '%c'\n", *input);
-        int aut_count = fsm_array_count(automata);
-        for (int i = 0; i < aut_count; ++i)
+        printf("NEXT WORD\n> ");
+        if ((input = fgets(inp, LINE_SIZE, stdin)) == NULL)
         {
-            printf("For automaton %d:\n", i);
-            struct FSM *aut = fsm_array_at(automata, i);
-            struct FSM_Array *new_aut = fsm_step(aut, *input);
+            printf("<exit>\n");
+            break;
+        }
+        automata = fsm_array_create();
+        fsm_array_add(automata, fsm_create(&spec));
+        inp[strcspn(inp, "\n")] = '\0';
 
-            if (new_aut != NULL)
+        int failed_count = 0;
+
+        while (*input)
+        {
+            printf("========\n");
+            printf("Step %d:\n", input - inp + 1);
+            printf("Passing character '%c'\n", *input);
+            int aut_count = fsm_array_count(automata);
+            for (int i = 0; i < aut_count; ++i)
             {
-                for (int j = 0; j < fsm_array_count(new_aut); ++j)
-                    fsm_array_add(automata, fsm_array_at(new_aut, j));
-                printf("Cloned, new %d automata\n", fsm_array_count(new_aut));
+                printf("For automaton %d:\n", i);
+                struct FSM *aut = fsm_array_at(automata, i);
+                struct FSM_Array *new_aut = fsm_step(aut, *input);
+
+                if (new_aut != NULL)
+                {
+                    for (int j = 0; j < fsm_array_count(new_aut); ++j)
+                        fsm_array_add(automata, fsm_array_at(new_aut, j));
+                    printf("Cloned, new %d automata\n", fsm_array_count(new_aut));
+                }
+                else
+                    printf("No new automata, state is now %d\n", fsm_get_state(aut));
             }
-            else
-                printf("No new automata, state is now %d\n", fsm_get_state(aut));
+            ++input;
         }
-        ++input;
-    }
-    printf("========\n");
-    for (int i = 0; i < fsm_array_count(automata); ++i)
-    {
-        struct FSM *aut = fsm_array_at(automata, i);
-        if (fsm_get_output(aut) == FSM_RECOGNIZED)
+        printf("========\n");
+        printf("Result: ");
+        for (int i = 0; i < fsm_array_count(automata); ++i)
         {
-            printf("Word has been recognized!\n");
-            goto cleanup;
+            struct FSM *aut = fsm_array_at(automata, i);
+            if (fsm_get_output(aut) == FSM_RECOGNIZED)
+            {
+                printf("%sWord has been recognized!\n%s", ANSI_GREEN, ANSI_RESET);
+                goto next;
+            }
         }
+        printf("%sWord has not been recognized!\n%s", ANSI_RED, ANSI_RESET);
+    next:
+        fsm_array_free(automata);
     }
-    printf("Word has not been recognized!\n");
 cleanup:
-    fsm_array_free(automata);
 
     free(spec.alphabet);
     fsm_states_free(spec.states);
---
+++ mydefs.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define ANSI_GREEN "\033[0;32m"
+#define ANSI_RED "\033[0;31m"
+#define ANSI_YELLOW "\033[0;33m"
+#define ANSI_RESET "\033[0;37m"
+
