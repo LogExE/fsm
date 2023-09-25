6ee882ded07d0b283f805d502e6614cf504b8802
LogExE <awesome149712@gmail.com>
2023-09-25 11:17:06 +0400

    more scaffolding

--- ndfsm_to_dfsm.c
+++ ndfsm_to_dfsm.c
@@ -2,10 +2,29 @@
 
 #include "fsm_spec.h"
 
+void convert_write(struct FSM_Spec spec, FILE *file)
+{
+    
+}
+
 int main(int argc, char **argv)
 {
-    printf("%d\n", argc);
-    for (int i = 0; i < argc; ++i)
-        printf("%d: %s\n", i, argv[i]);
+    if (argc != 3)
+    {
+        fprintf(stderr, "usage: %s <file_in> <file_out>\n", argv[0]);
+        return 1;
+    }
+    FILE *file_in = fopen(argv[1], "r");
+    if (file_in == NULL)
+    {
+        fprintf(stderr, "file_in doesn't exist!\n");
+        return 1;
+    }
+    struct FSM_Spec spec;
+    fsm_spec_read_from(file_in, &spec);
+    fclose(file_in);
+    FILE *file_out = fopen(argv[2], "w");
+    convert_write(spec, file_out);
+    fclose(file_out);
     return 0;
 }
